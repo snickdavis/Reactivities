9f9229527165ef335da198b68e999b3904bbbd8a
ndavis <Nick.Davis@Verint.com>
2022-02-15 14:50:16 +0000

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,62 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
 
-namespace API
-{
-    public class Startup
-    {
+namespace API {
+    public class Startup {
+        private const string _corsPolicy = "CorsPolicy";
         private readonly IConfiguration _config;
-        public Startup(IConfiguration config)
-        {
+        public Startup(IConfiguration config) {
             this._config = config;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
+            services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
 
-            services.AddDbContext<DataContext>(opt => { 
+            services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt => {
+                opt.AddPolicy(_corsPolicy, policy => {
+                    policy.AllowAnyMethod().WithHeaders("Authorization").WithOrigins("http://localhost:3000");
+                });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
             // app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseCors(_corsPolicy);
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
             });
         }
